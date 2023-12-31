8fed0d4a8a1c58c4de8a2ad084a21bdb5dbe6eed
neojarvis <techadmin@iamneo.ai>
2023-12-31 05:11:00 +0000

    Manual-save-commit-12/31/2023--10:40:56-AM

--- springapp/src/main/java/com/example/springapp/Calculator.java
+++ springapp/src/main/java/com/example/springapp/Calculator.java
@@ -3,5 +3,10 @@ public interface Calculator {
 
 
 //Write Your Code Here
+public static void main(String[] args) {
+    System.out.println("welcome");
+    System.out.println("qwerty");
+    
+}
 
 }
